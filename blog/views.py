06f0bacca48cfbb1da459ebd37e2c4a4e55c233e
Nick1238 <121309591+Nick1238@users.noreply.github.com>
2023-06-02 11:20:49 +0300

    Add blog templates, url config

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from blog.models import Post
+
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,
+                  'post/list.html',
+                  {'posts': posts})
+
+def post_detail(request, ind):
+    post = get_object_or_404(Post,
+                             id=ind)
+
+    return render(request,
+                  'post/detail.html',
+                  {'post': post})
